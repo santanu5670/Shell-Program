35d575002ca244481f89682ac066927dd4c938e2
Santanu Saha <santanu2539@gmail.com>
2020-10-02 21:53:13 +0530

    A Year is leapyear or not

---
+++ leapyear.sh
@@ -0,0 +1,11 @@
+echo "Enter The Year"
+read y
+if [ `expr $y % 4` -eq 0 -a `expr $y % 100` -ne 0 ]
+then
+echo "The Year is Leap Year"
+elif [ `expr $y % 400` -eq 0 ]
+then
+echo "The Year is Leap Year"
+else
+echo "The Year is Not Leap Year"
+fi
