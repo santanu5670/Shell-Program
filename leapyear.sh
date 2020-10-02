f49781fe02d52874b093891b53a6527465108981
Santanu Saha <santanu2539@gmail.com>
2020-10-02 22:38:31 +0530

    I modified it

--- leapyear.sh
+++ leapyear.sh
@@ -2,7 +2,7 @@ echo "Enter The Year:"
 read y
 if [ `expr $y % 4` -eq 0 -a `expr $y % 100` -ne 0 ]
 then
-echo "The Year is Leap Year:"
+echo "The Year is Leap Year"
 elif [ `expr $y % 400` -eq 0 ]
 then
 echo "The Year is Leap Year"
